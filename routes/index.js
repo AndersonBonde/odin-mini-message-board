d583d05e6fc5011036f3c3a814a7328d6df36387
Anderson <andersonbonde@gmail.com>
2024-08-15 12:09:25 -0300

    Change from hardcoded messages to database entries

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
+const db = require('../database/queries');
 
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  }
-];
+router.get('/', async (req, res) => {
+  const messages = await db.getAllMessages();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board', messages, });
+  res.render('index', { title: 'Mini Message Board', messages: messages, });
 });
 
 router.get('/new', (req, res, next) => {
